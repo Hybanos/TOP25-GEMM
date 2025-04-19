2c0b7363a21e7932cf6a39d399a3a9e07263677b
Gautier <Hybanos@orange.fr>
2025-04-19 11:51:50 +0200

    i need a sanity check

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
 
 import os
+import math
 import subprocess
 
 env = os.environ.copy()
@@ -32,15 +33,57 @@ def strong_scaling():
 
     # plot
     plt.plot(x, y, label='Speedup')
-    plt.plot(np.arange(1, x[-1], 0.1), np.arange(1, x[-1], 0.1), color="gray", linestyle='dashed')
+    plt.plot(np.arange(1, x[-1], 0.1), np.arange(1, x[-1], 0.1), color="gray", linestyle='dashed', label="Ideal speedup")
     plt.grid(True, which="both", ls="--")
     plt.xscale("log", base=2)
     plt.ylim(0)
     plt.gca().xaxis.set_major_formatter(FuncFormatter(lambda x, _: f'{int(x)}'))
     plt.xlabel("Number of cores")
     plt.ylabel("Speedup")
+    plt.legend()
+    plt.title("GEMM strong scaling")
+    plt.tight_layout()
     plt.savefig("strong_scaling.png")
 
+def weak_scaling():
+    x = []
+    y = []
+
+    op_per_cpu = 1024 * 1024 * 1024 * 1.3
+
+    # runs
+    for cpus in range(1, MAX_CPUS):
+        # that shoud give a ~constant amount of operations per core
+        load = math.cbrt(op_per_cpu * cpus)
+        print(load)
+        env["OMP_NUM_THREADS"] = str(cpus)
+        tmp = subprocess.Popen(
+            ["build/src/top.matrix_product", f"{load}", f"{load}", f"{load}"], 
+            stdout=subprocess.PIPE,
+            env=env).stdout.read().decode().strip().split("\t")
+        print(tmp)
+        x.append(int(tmp[3]))
+        y.append(float(tmp[4]))
+    
+    print(x, y)
+    # calc
+    initial_time = y[0]
+    y = initial_time / np.array(y)
+
+    # plot
+    plt.plot(x, y, label='Efficiency')
+    plt.hlines([1], 1, x[-1], color="gray", linestyle="dashed", label="Ideal efficiency")
+    plt.grid(True, which="both", ls="--")
+    plt.xscale("log", base=2)
+    plt.ylim(0)
+    plt.gca().xaxis.set_major_formatter(FuncFormatter(lambda x, _: f'{int(x)}'))
+    plt.xlabel("Number of cores")
+    plt.ylabel("Efficiency")
+    plt.legend()
+    plt.title("GEMM weak scaling")
+    plt.tight_layout()
+    plt.savefig("weak_scalint.png")
 
 if __name__ == "__main__":
-    strong_scaling()
+    # strong_scaling()
+    weak_scaling()
